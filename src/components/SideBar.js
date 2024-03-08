8a144b53ea8b582d8174b1c0e1fff5e1f3f38fb7
vyomgangwar25 <vyomgangwar25@gmail.com>
2024-03-08 19:47:25 +0530

    add icons

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -1,4 +1,11 @@
 import React from 'react'
+import { AiFillHome, AiFillLike,RiVideoFill } from "react-icons/ai";
+import {
+  MdSubscriptions,
+  MdVideoLibrary,
+  MdHistory,
+  MdOutlineWatchLater,
+} from "react-icons/md";
 import { useSelector } from 'react-redux'
 import { Link } from 'react-router-dom'
 
@@ -8,29 +15,46 @@ const SideBar = () => {
   if(!isMenuOpen)
   return null
   return (
-    <div className='p-9 mx-5 shadow-lg '>
-     <ul className="w-50">
-     <Link to="/"> <li>Home</li></Link>
-       <Link to="https://www.youtube.com/results?search_query=shorts"> <li>Shorts</li></Link>
-        <li>Videos</li>
-        <li>Live</li>
-      </ul>
-      <h1 className='font-bold pt-5'>Subscription</h1>
-
-      <ul>
-        <li>Music</li>
-        <li>Sports</li>
-        <li>Gaming</li>
-        <li>Movies</li>
-      </ul>
-      <h1 className='font-bold pt-5'>Watch Later</h1>
-      <ul>
-        <li>Music</li>
-        <li>Sports</li>
-        <li>Gaming</li>
-        <li>Movies</li>
-      </ul>
-    </div>
+    <div className='p-9 mx-5 shadow-lg'>
+  <ul className="w-50">
+    <Link to="/">
+      <li className='flex items-center  font-bold mb-4'>
+        <AiFillHome className='mr-4 text-xl' />
+        Home
+      </li>
+    </Link>
+    <Link to="/">
+      <li className='flex items-center  font-bold mb-4'>
+        <MdSubscriptions className='mr-4 text-xl' />
+        Subscription
+      </li>
+    </Link>
+    <Link to="/">
+      <li className='flex items-center  font-bold mb-4'>
+        <MdVideoLibrary className='mr-4 text-xl' />
+        Library
+      </li>
+    </Link>
+    <Link to="/">
+      <li className='flex items-center font-bold mb-4'>
+        <MdHistory className='mr-4 text-xl' />
+        History
+      </li>
+    </Link>
+    <Link to="/">
+      <li className='flex items-center   font-bold mb-4'>
+        <MdOutlineWatchLater className='mr-4 text-xl' />
+        Watch Later
+      </li>
+    </Link>
+    <Link to="/">
+      <li className='flex items-center   font-bold mb-4'>
+        <AiFillLike className='mr-4 text-xl' />
+        Liked Videos
+      </li>
+    </Link>
+  </ul>
+</div>
   )
 }
 
